c4e389dcc79e15713b224d30987650e061a77d49
samuel <samuellpvt@gmail.com>
2021-12-29 15:12:45 -0300

    nearest working

---
+++ class.py
@@ -0,0 +1,119 @@
+
+
+from random import seed, random
+#this class can represent both a point or a hyperplane
+#a leaf is always a point and the nodes in between are always hyperplanes
+#if it is a hyperplane the "point" atribute represents the median
+#of the dimension  that was split 
+class node(object):
+    def __init__(self,left , right ,point ,dim,is_leaf):
+        self.left = left 
+        self.right = right 
+        self.point = point 
+        self.dim =dim
+        self.is_leaf = is_leaf
+        
+def euc(a,b):
+    sum=0
+    for i in range(len(a)):
+        temp = a[i] - b[i]
+        sum += temp*temp
+    return sum**0.5
+
+class kdtree(object):
+    def __init__(self, points,dimension):    
+        def __build(points,dimension,i=0):
+            if len(points)==1:
+                print(points[0])
+                return  node(None,None,points[0],i,True)
+            else:
+                # i= (i+1)%dimension
+                i= i%dimension
+                points.sort(key=lambda x: x[i])
+                
+                half = len(points) >> 1
+                print(points[:half],points[half],points[half:],i)
+               
+                v_right = __build(points[:half],dimension,i+1)
+                v_left = __build(points[half:],dimension,i+1)
+                return node(v_left,v_right,points[half][i],i,False)
+    
+        self.root = __build(points,dimension)   
+
+    def nearest(self, test):
+        def __near(node,test,closest_p,closest_d):
+            
+            if node == None:
+                return (closest_p,closest_d)   
+            if node.is_leaf :
+                
+                dist = euc( node.point,test )
+                print(node.point)
+                if(dist < closest_d):
+                    closest_p = node.point
+                    closest_d =dist
+                    return (closest_p,closest_d)                   
+                else:
+                    return(closest_p,closest_d)   
+            else:
+                #chooses if the dimension of the test point
+                #is greater or smaller than the median of the split hyper plane
+
+                if(node.point >= test[node.dim]):
+                    
+                    p=__near(node.left,test,closest_p,closest_d)
+                else:
+                    
+                    p= __near(node.right,test,closest_p,closest_d)
+                closest_p =p[0]
+                closest_d =p[1]
+                radius =  abs( closest_p[node.dim]- test[node.dim])
+                cp_to_plane =abs( closest_p[node.dim]- node.point) 
+                if (radius < cp_to_plane ):
+                    return p
+                if(node.point >= test[node.dim]):
+                    
+                    p=__near(node.right,test,closest_p,closest_d)
+                else:
+                    
+                    p= __near(node.left,test,closest_p,closest_d)
+                
+                return p
+
+            
+
+               
+                
+               
+
+        temp_point=(0,0)
+        print(__near(self.root,test,temp_point,100000000))
+       
+                
+
+
+    def print(self):
+        def print_h(Node):
+            if (Node.is_leaf):
+                print("leaf" , Node.point)
+            else:
+                print("mediana :" , Node.point ,"dimensao :",Node.dim)
+               
+                print_h(Node.left)
+                print_h(Node.right)
+          
+
+        print_h(self.root)
+
+# tree= kdtree ([(2,3),(5,4),(9,6),(4,7),(8,1),(7,2)],2)
+# tree = kdtree ([(1,1),(2,2),(3,3),(4,4),(5,5)],2)
+
+a=[(100,100)]
+for i in range(5):
+    a.append((random(),random()))
+
+
+
+tree = kdtree(a,2)
+tree.print()
+tree.nearest((51,51))
---
+++ readme.md
@@ -0,0 +1,3 @@
+para criar a arvore kd o único problema é a pegar a mediana:
+- tentar com o presort
+- sem o presort
